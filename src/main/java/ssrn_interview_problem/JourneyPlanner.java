f5e21d89335c08ab42db007b20d478dce13e2c4a
Smith, Stefan A. (ELS-OXF) <s.smith@elsevier.com>
2017-09-18 22:41:18 +0100

    De-duplicate creating train instances

--- src/main/java/ssrn_interview_problem/JourneyPlanner.java
+++ src/main/java/ssrn_interview_problem/JourneyPlanner.java
@@ -2,26 +2,27 @@ package ssrn_interview_problem;
 
 import org.joda.time.LocalTime;
 import org.joda.time.Minutes;
-import org.joda.time.format.DateTimeFormat;
 
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 class JourneyPlanner {
-    private final String[][] timetable;
     private final List<String> stations;
+    private final List<Train> trains;
 
     JourneyPlanner(String[][] timetable) {
-        this.timetable = timetable;
         stations = Arrays.asList(timetable[0]);
+        trains = Arrays.stream(timetable)
+                .skip(1)
+                .map(trainTimes -> new Train(trainTimes, stations))
+                .collect(Collectors.toList());
     }
 
     int getDurationBetween(LocalTime journeyStartTime, String departureStation, String arrivalStation) {
-
-        Train firstAvailableTrainTimetable = Arrays.stream(timetable)
-                .skip(1)
-                .map(trainTimes -> new Train(trainTimes, stations))
+        Train firstAvailableTrainTimetable = trains.stream()
                 .filter(train -> !train.getTimeAt(departureStation).isBefore(journeyStartTime))
                 .findFirst()
                 .get();
@@ -36,10 +37,7 @@ class JourneyPlanner {
     }
 
     String getFastestTrainBetween(String departureStation, String arrivalStation) {
-
-        Train fastestTrainTimetable = Arrays.stream(timetable)
-                .skip(1)
-                .map(trainTimes -> new Train(trainTimes, stations))
+        Train fastestTrainTimetable = trains.stream()
                 .sorted(Comparator.comparing(train -> Minutes.minutesBetween(train.getTimeAt(departureStation), train.getTimeAt(arrivalStation))))
                 .findFirst()
                 .get();
