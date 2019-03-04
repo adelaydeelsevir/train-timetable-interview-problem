b83575e9c59eaccef4b352081ec379fee09425bf
Michael B. Harris <m.harris@elsevier.com>
2019-03-04 09:55:17 +0000

    Add solution to code problem.

---
+++ solutions/ssrn-interview-problem-solution-mbh/src/main/java/refactoring/RouteTimeTable.java
@@ -0,0 +1,50 @@
+package refactoring;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class RouteTimeTable {
+    private final String[][] runningTimes;
+    private final List<String> stationNames;
+    private final List<Train> trains;
+
+    RouteTimeTable(String[][] routeTimeTable) {
+        this.runningTimes = Arrays.copyOfRange(routeTimeTable, 1, routeTimeTable.length);
+        this.stationNames = Arrays.asList(routeTimeTable[0]);
+        this.trains = getTrainsRunningOnRoute();
+    }
+
+    private List<Train> getTrainsRunningOnRoute() {
+        List<Train> trains = new ArrayList<Train>();
+        for (String[] trainTimes : this.runningTimes) {
+            trains.add(new Train(trainTimes, this.stationNames));
+        }
+        return trains;
+    }
+
+    public Train getNextAvailableTrain(String startStationName, LocalTime currentTime) {
+
+        for (Train train : this.trains) {
+            if (train.hasNotYetDeparted(startStationName, currentTime)) {
+                return train;
+            }
+        }
+        return null;
+    }
+
+    public HashMap<Integer, List<String>> getJourneyTimesForRoute(String startStationName, String endStationName) {
+        HashMap<Integer, List<String>> journeysByDuration = new HashMap<>();
+        for (Train train : this.trains) {
+
+            Integer duration = train.journeyTimeBetween(startStationName, endStationName);
+            String startTimeString = train.getTrainStartTimeAsString(startStationName);
+            journeysByDuration.computeIfAbsent(duration, integer -> new ArrayList<>()).add(startTimeString);
+
+        }
+        return journeysByDuration;
+    }
+}
+
