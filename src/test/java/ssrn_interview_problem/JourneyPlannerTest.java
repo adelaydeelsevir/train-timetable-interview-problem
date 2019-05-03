ad093b25f7b7ecf6b680acb6c7f233ea0addb22e
Harris, Michael (ELS-OXF) <m.harris@elsevier.com>
2019-05-03 15:14:08 +0000

    Upload New File

---
+++ src/test/java/ssrn_interview_problem/JourneyPlannerTest.java
@@ -0,0 +1,19 @@
+package ssrn_interview_problem;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class JourneyPlannerTest {
+
+    @Test
+    public void shouldReportDurationOfJourneyBetweenTwoStations() {
+        JourneyPlanner journeyPlanner = new JourneyPlanner();
+
+        int duration = journeyPlanner.durationOfJourneyStartingAt("0907", "Camborne", "Exeter St Davids");
+
+        assertThat(duration, is(equalTo(150)));
+    }
+}
