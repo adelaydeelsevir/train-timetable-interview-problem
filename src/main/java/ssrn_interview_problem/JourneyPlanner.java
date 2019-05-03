2d12f472e55ad76e37a9afa0bcdb99e774c9d552
Mike Harris <m.harris@elsevier.com>
2019-05-03 16:22:12 +0100

    Add JourneyPlanner class

---
+++ src/main/java/ssrn_interview_problem/JourneyPlanner.java
@@ -0,0 +1,8 @@
+package ssrn_interview_problem;
+
+public class JourneyPlanner {
+
+    public int durationOfJourneyStartingAt(String arriveAtStationTime, String startStationName, String endStationName) {
+        return -1;
+    }
+}
