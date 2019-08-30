a98e4ad8ece1f1eedec1c6ac2863a23993d0a0ae
Mike Harris <m.harris@elsevier.com>
2019-08-30 12:54:42 +0100

    Provide more stubs for tests as a guideline

--- src/test/java/ssrn_interview_problem/JourneyPlannerTest.java
+++ src/test/java/ssrn_interview_problem/JourneyPlannerTest.java
@@ -1,19 +1,72 @@
 package ssrn_interview_problem;
 
-import org.junit.Test;
-
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
+
+import org.junit.Ignore;
+import org.junit.Test;
 
 public class JourneyPlannerTest {
 
     @Test
     public void shouldReportDurationOfJourneyBetweenTwoStations() {
+        // Given
         JourneyPlanner journeyPlanner = new JourneyPlanner(TestData.TIMETABLE);
 
+        // When
         int duration = journeyPlanner.durationOfJourneyStartingAt("0907", "Camborne", "Exeter St Davids");
 
+        // Then
         assertThat(duration, is(equalTo(150)));
     }
+
+    @Test
+    public void shouldReportDurationForFirstAvailableTrain() {
+        // Given
+        JourneyPlanner journeyPlanner = new JourneyPlanner(TestData.TIMETABLE);
+
+        // When
+        int duration = journeyPlanner.durationOfJourneyStartingAt("1023", "Camborne", "Exeter St Davids");
+
+        // Then
+        assertThat(duration, is(equalTo(159)));
+    }
+
+    @Test
+    @Ignore
+    public void shouldReportDurationIncludingWaitingTimeOnPlatform() {
+        // Given
+        JourneyPlanner journeyPlanner = new JourneyPlanner(TestData.TIMETABLE);
+
+        // When
+        int duration = journeyPlanner.durationOfJourneyStartingAt("1101", "St Austell", "Par");
+
+        // Then
+        assertThat(duration, is(equalTo(56)));
+    }
+
+    @Test
+    @Ignore
+    public void shouldReportFastestTrainBetweenTwoStations() {
+        // Given
+        JourneyPlanner journeyPlanner = new JourneyPlanner(TestData.TIMETABLE);
+
+        // When I try to find the fastest train between "Exeter St Davids" and "London Paddington"
+
+        // Then I find the next train departs at "1357"
+    }
+
+    @Test
+    @Ignore
+    public void shouldReportEarliestRouteWhenThereIsATieBetweenFastestTrains() {
+        // Given
+        JourneyPlanner journeyPlanner = new JourneyPlanner(TestData.TIMETABLE);
+
+        // When
+
+        // Then
+    }
+
+
 }
