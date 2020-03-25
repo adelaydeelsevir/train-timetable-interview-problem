d5d2c3f63401373ca43f15f833971330cc0d2076
Mike Harris <m.harris@elsevier.com>
2020-03-25 15:22:06 +0000

    Optimised imports

--- src/test/java/train_timetable_interview_problem/JourneyPlannerTest.java
+++ src/test/java/train_timetable_interview_problem/JourneyPlannerTest.java
@@ -1,11 +1,10 @@
 package train_timetable_interview_problem;
 
+import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
-
-import org.junit.Ignore;
-import org.junit.Test;
 
 public class JourneyPlannerTest {
 
